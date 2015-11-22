8a46451cfe988a30292860d92cb74b640711e0e7
Vasisht, Sunil S <Sunil.Vasisht@capitalone.com>
2015-11-22 15:03:56 -0600

    add toString()

--- pa4/java/src/cs224n/deep/Datum.java
+++ pa4/java/src/cs224n/deep/Datum.java
@@ -1,13 +1,19 @@
 package cs224n.deep;
+
 import java.util.*;
 
 public class Datum {
 
-  public final String word;
-  public final String label;
-  
-  public Datum(String word, String label) {
-    this.word = word;
-    this.label = label;
-  }
+	public final String word;
+	public final String label;
+
+	public Datum(String word, String label) {
+		this.word = word;
+		this.label = label;
+	}
+
+	@Override
+	public String toString() {
+		return word + "(" + label + ")";
+	}
 }
