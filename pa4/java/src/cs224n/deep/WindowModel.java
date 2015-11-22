ae1d7947808d49acb4b114315e1dbbe3c801c679
Vasisht, Sunil S <Sunil.Vasisht@capitalone.com>
2015-11-22 12:00:32 -0600

    added helper methods to deal with inputs

--- pa4/java/src/cs224n/deep/WindowModel.java
+++ pa4/java/src/cs224n/deep/WindowModel.java
@@ -16,7 +16,7 @@ public class WindowModel {
 
 	public WindowModel(int _windowSize, int _hiddenSize, double _lr){
 		//TODO
-		windowSize = _windowSize;
+		windowSize = _windowSize; // assuming odd window size
 		hiddenSize = _hiddenSize;
 		wordSize = 50; // 50 dim word vector
 		outputNodes = 5; // number of NER outputs
@@ -37,6 +37,59 @@ public class WindowModel {
 		W = initRandom(hiddenSize, windowSize*wordSize + 1); // first column is bias
 		U = initRandom(outputNodes, hiddenSize+1); // first column is bias
 		
+	}
+	/**
+	 * 
+	 * @param words List of words in a sentence that needs to be padded
+	 * @return List of padded words
+	 */
+	public List<String> pad(List<String> words) {
+		List<String> padded = new ArrayList<String>();
+		int padLen = windowSize / 2;
+		for (int i=0; i<padLen; i++) {
+			padded.add("<s>");
+		}
+		padded.addAll(words);
+		for (int i=0; i<padLen; i++) {
+			padded.add("</s>");
+		}
+		return padded;
+	}
+	/**
+	 * 
+	 * @param paddedWords List of padded words in a sentence
+	 * @return List of a windowed words, each window is a List of words
+	 */
+	public List<List<String>> window(List<String> paddedWords) {
+		List<List<String>> windows = new ArrayList<List<String>>();
+		int padLen = windowSize / 2;
+		for (int index = padLen; index < (paddedWords.size()-padLen); index++) {
+			List<String> window = new ArrayList<String>();
+			for (int offset = -padLen; offset <= padLen; offset++) {
+				window.add(paddedWords.get(index+offset));
+			}
+			windows.add(window);
+		}
+		return windows;
+	}
+	/**
+	 * 
+	 * @param one window of sentence words
+	 * @return vector representation of of concatenated words in the window
+	 */
+	public SimpleMatrix toInputVector(List<String> window) {
+		SimpleMatrix X = new SimpleMatrix(windowSize*wordSize +1, 1); // The first row = 1, to deal with bias
+		int row = 0;
+		X.set(row++, 0, 1.0);
+		for (String word : window) {
+			int index = FeatureFactory.wordToNum.get(word);
+			SimpleMatrix wordVec = FeatureFactory.allVecs.extractVector(false,
+					index); // extract column vector at row=index
+			X.insertIntoThis(row, 0, wordVec);
+			row += wordVec.getNumElements(); // increment row to pint to next
+												// slot to insert
+		}
+		return X;
 	}
 
 
