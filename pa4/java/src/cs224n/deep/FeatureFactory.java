89ad79d334f60830c748e15f359f2e1fee1e6caa
Jie Shen <jie@Jies-MacBook-Pro.local>
2015-11-21 23:48:03 -0600

    add funcs to load word vectors

--- pa4/java/src/cs224n/deep/FeatureFactory.java
+++ pa4/java/src/cs224n/deep/FeatureFactory.java
@@ -53,18 +53,59 @@ public class FeatureFactory {
 	// Look up table matrix with all word vectors as defined in lecture with dimensionality n x |V|
 	static SimpleMatrix allVecs; //access it directly in WindowModel
 	public static SimpleMatrix readWordVectors(String vecFilename) throws IOException {
-		if (allVecs!=null) return allVecs;
-		return null;
-		//TODO implement this
-		//set allVecs from filename		
-
+            
+            if (allVecs!=null) return allVecs;
+             
+            int numCols = wordToNum.size();
+            int numRows = 0;
+            int vecNumCols = 0;
+            //read file
+            BufferedReader in = new BufferedReader(new FileReader(vecFilename));
+            int iCol = 0;
+            double[][] wordVectors = null;
+            for (String line = in.readLine(); line != null; line = in.readLine()) {
+                if (line.trim().length() == 0) {
+                        continue;
+                }
+                String[] words = line.split("\\s+");
+                
+                //count the row dimension
+                if (wordVectors == null){
+                     numRows = words.length;
+                     wordVectors = new double[numRows][numCols];
+                }
+                
+                for (int iRow = 0; iRow < numRows; iRow++){
+                    wordVectors[iRow][iCol] = Double.parseDouble(words[iRow]);
+                }
+                vecNumCols++;
+                        
+            }
+            
+            // change if the wordvectors and vocab has the same dimension
+            if (vecNumCols!= numCols)
+                System.err.println("Unmatched word vectors dimensions");
+            
+            // System.out.println("ncol = " + numCols + ", nrow = " + numRows);
+            allVecs = new SimpleMatrix(wordVectors);
+            return allVecs;    
 	}
 	// might be useful for word to number lookups, just access them directly in WindowModel
 	public static HashMap<String, Integer> wordToNum = new HashMap<String, Integer>(); 
 	public static HashMap<Integer, String> numToWord = new HashMap<Integer, String>();
 
 	public static HashMap<String, Integer> initializeVocab(String vocabFilename) throws IOException {
-		//TODO: create this
+            BufferedReader in = new BufferedReader(new FileReader(vocabFilename));
+            int index = 0;
+            for (String line = in.readLine(); line != null; line = in.readLine()) {
+                    if (line.trim().length() == 0) {
+                            continue;
+                    }
+                    String word = line.trim();
+                    wordToNum.put(word, index);
+                    numToWord.put(index,word);
+                    index++;
+            }        
 		return wordToNum;
 	}
  
