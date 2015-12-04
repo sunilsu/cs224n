ab189f4caa259f9658b6109d7a26581d40f6889b
Vasisht, Sunil S <Sunil.Vasisht@capitalone.com>
2015-12-03 21:51:28 -0600

    rerun plots with lr = 0.03 and C = 0.001 for both given word vecs and rnd word vecs.

--- pa4/vis/word_vis.py
+++ pa4/vis/word_vis.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Nov 28 16:19:40 2015
 
-@author: yyo662
-"""
 import pandas as pd
 from sklearn.manifold import TSNE
 import matplotlib
@@ -49,7 +45,7 @@ tsne_old = tsne.fit_transform(vec_label[xcols])
 plt.scatter(tsne_old[:,0],tsne_old[:,1], c=vec_label.colors)
 
 # read in new vectors after training
-newVec = pd.read_csv('newVectors.txt', sep=' ', header=None)
+newVec = pd.read_csv('newRndVectors.txt', sep=' ', header=None)
 newVec = newVec.iloc[:, :-1]
 
 new_vec_label = vocab_index.join(newVec)
@@ -60,3 +56,52 @@ tsne = TSNE(random_state=37153)
 tsne_new = tsne.fit_transform(new_vec_label[xcols])
 plt.scatter(tsne_new[:,0],tsne_new[:,1], c=new_vec_label.colors)
 
+flag_no = (tsne_new[:,0] < 0) & (tsne_new[:,1] > 0) & (new_vec_label.colors != 'w')
+vec_label.ix[flag_no,['word','colors']].values
+'''
+Vectors
+array([['and', 'r'],
+       ['in', 'r'],
+       ['in', 'r'],
+       ['for', 'r'],
+       ['as', 'r'],
+       ['on', 'g'],
+       ["'s", 'r'],
+       [':', 'g'],
+       ['said', 'b'],
+       ['man', 'b'],
+       ['across', 'r'],
+       ['mother', 'b'],
+       ['ca', 'c'],
+       ['centers', 'r'],
+       ['jan', 'b'],
+       ['ally', 'b'],
+       ['sterling', 'b'],
+       ['f.a.', 'g']], dtype=object)
+'''
+flag_no = (tsne_new[:,0] >0) & (tsne_new[:,1] < -2) & (new_vec_label.colors != 'w')
+vec_label.ix[flag_no,['word','colors']].values
+
+'''
+rndVectors
+array([['the', 'r'],
+       ['the', 'c'],
+       ['the', 'b'],
+       ['in', 'r'],
+       ['in', 'r'],
+       ['on', 'g'],
+       ['two', 'r'],
+       ['school', 'c'],
+       ['play', 'g'],
+       ['per', 'b'],
+       ['foreign', 'r'],
+       ['miles', 'b'],
+       ['key', 'b'],
+       ['spring', 'b'],
+       ['price', 'b'],
+       ['criminal', 'r'],
+       ['champions', 'g'],
+       ['centers', 'r'],
+       ['add', 'r'],
+       ['erickson', 'b']], dtype=object)
+'''
